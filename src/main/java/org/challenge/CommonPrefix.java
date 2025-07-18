9d168e354b4f5fc36bb8c4e264ec2ccfeae7cdcd
vagrant <dreadfiles@hootmail.com>
2025-07-18 16:07:01 -0500

    Up Down Cards Challenge

--- src/main/java/org/challenge/CommonPrefix.java
+++ src/main/java/org/challenge/CommonPrefix.java
@@ -1,6 +1,6 @@
 package org.challenge;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class CommonPrefix {
 
@@ -8,10 +8,21 @@ public class CommonPrefix {
         System.out.println("Start");
 
         String paragraph = "The world  is     yours";
-        String[] splitParagraph = paragraph.split("\\s+");
 
-        int wordCounter = splitParagraph.length;
-        System.out.println("wordCounter = " + wordCounter);
+        String[] splitParagraph = paragraph.split(" ");
+        System.out.println("splitParagraph = " + Arrays.toString(splitParagraph));
+
+        String[] splitParagraphRegex = paragraph.split("\\s+");
+        System.out.println("splitParagraphRegex = " + Arrays.toString(splitParagraphRegex));
+
+        String[] splitParagraphRegex2 = paragraph.split("\\s{2,}");
+        System.out.println("splitParagraphRegex2 = " + Arrays.toString(splitParagraphRegex2));
+
+        String[] splitParagraphRegex3 = paragraph.split("\\s{1,}");
+        System.out.println("splitParagraphRegex3 = " + Arrays.toString(splitParagraphRegex3));
+
+        String[] splitParagraphRegex4 = paragraph.split("\\s*");
+        System.out.println("splitParagraphRegex4 = " + Arrays.toString(splitParagraphRegex4));
 
         System.out.println("End");
     }
