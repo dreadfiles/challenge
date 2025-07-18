006d6db086a46d07942f20f17ee6d6b26bb7658a
vagrant <dreadfiles@hootmail.com>
2025-07-18 15:39:45 -0500

    Up Down Cards Challenge

--- src/main/java/org/challenge/UpDownCards.java
+++ src/main/java/org/challenge/UpDownCards.java
@@ -1,15 +1,15 @@
 package org.challenge;
 
-public class Dei {
+import java.util.LinkedList;
+import java.util.List;
+
+public class UpDownCards {
 
     public static void main(String[] args) {
-        /*Mutable s1=0;
         List<String> list = List.of("5 de diamantes", "3 de picas", "1 de corazon", "as de corazones", "4 de treboles", "3 de treboles");
-        //"3 de treboles","as de corazones", 3 de picas", "5 de diamantes", "1 de corazon","4 de treboles"
         LinkedList<String> link = new LinkedList<>();
         list.forEach(c -> {
             int index = list.indexOf(c);
-            //Integer a
             if (index == 0) {
                 link.add(c);
             } else if (index % 2 != 0) {
@@ -19,9 +19,6 @@ public class Dei {
             }
         });
 
-
-        list.stream().filter(s->s.equals(s1)).toList();
-        System.out.println("link = " + link);*/
-        System.out.println("Hello, World!");
+        System.out.println("link = " + link);
     }
 }
